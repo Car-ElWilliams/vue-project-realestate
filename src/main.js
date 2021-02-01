182ec347323753e18523f7709bc382150c8dbbce
Carel Williams <carel@live.se>
2021-02-01 12:06:26 +0100

    Adds new icons to the the project Removes services workers that were not build with vue cli Commented out sw.js instead of deleting it

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
-import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import "./sw.js";
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
--- vue.config.js
+++ vue.config.js
@@ -7,3 +7,5 @@ module.exports = {
     themeColor: 'whitesmoke'
   }
 }
+
+np
